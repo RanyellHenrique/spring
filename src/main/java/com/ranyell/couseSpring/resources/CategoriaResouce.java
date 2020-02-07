f684cd0ffd7b6774cdd6fb9f01d1b36f3853d23b
RanyellHenrique <ranyellhenrique@gmail.com>
2020-02-07 00:31:41 -0300

    REST testado

---
+++ src/main/java/com/ranyell/couseSpring/resources/CategoriaResouce.java
@@ -0,0 +1,16 @@
+package com.ranyell.couseSpring.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResouce {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		return "Rest está funcionando";
+	}
+
+}
