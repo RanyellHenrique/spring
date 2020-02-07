61b6762378f450ba50bd3fa4f6af360ef70b0974
RanyellHenrique <ranyellhenrique@gmail.com>
2020-02-07 00:51:32 -0300

    Testando a primeira classe de dominio

--- src/main/java/com/ranyell/couseSpring/resources/CategoriaResouce.java
+++ src/main/java/com/ranyell/couseSpring/resources/CategoriaResouce.java
@@ -1,16 +1,30 @@
 package com.ranyell.couseSpring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ranyell.couseSpring.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResouce {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+	
+		List<Categoria> lista = new ArrayList<>();
+		
+		Categoria c1 = new Categoria(1, "Anna");
+		Categoria c2 = new Categoria(2, "Lucas");
+		
+		lista.add(c1);
+		lista.add(c2);
+		
+		return lista;
 	}
 
 }
