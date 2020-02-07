85388709a321fbc3be7d2b129ec0215b79384db4
RanyellHenrique <ranyellhenrique@gmail.com>
2020-02-07 13:36:45 -0300

    Criando oprcacao de instaciacao

--- src/main/java/com/ranyell/couseSpring/CourseSpringApplication.java
+++ src/main/java/com/ranyell/couseSpring/CourseSpringApplication.java
@@ -1,13 +1,34 @@
 package com.ranyell.couseSpring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ranyell.couseSpring.domain.Categoria;
+import com.ranyell.couseSpring.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CourseSpringApplication {
+public class CourseSpringApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria c1 = new Categoria(null, "Informática");
+		Categoria c2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(c1, c2));
+		
+		
 	}
 
 }
