4324d335b8cb4790e9f9a0a61da1b0f7ee7e1ad9
RanyellHenrique <ranyellhenrique@gmail.com>
2020-02-07 14:42:45 -0300

    Ajustes end point categorias

--- src/main/java/com/ranyell/couseSpring/domain/Categoria.java
+++ src/main/java/com/ranyell/couseSpring/domain/Categoria.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -19,6 +21,7 @@ public class Categoria implements Serializable {
 	public Integer id;
 	public String nome;
 	
+	@JsonManagedReference
 	@ManyToMany(mappedBy = "categorias")
 	List<Produto> produtos = new ArrayList<>();
 	
--- src/main/java/com/ranyell/couseSpring/services/CategoriaService.java
+++ src/main/java/com/ranyell/couseSpring/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.ranyell.couseSpring.domain.Categoria;
 import com.ranyell.couseSpring.repositories.CategoriaRepository;
+import com.ranyell.couseSpring.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,9 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", tipo: " + Categoria.class.getName()));
+		
 	}
 
 }
