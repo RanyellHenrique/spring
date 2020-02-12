9dcd9411130754f4307f9619663d739fa189ab07
RanyellHenrique <ranyellhenrique@gmail.com>
2020-02-12 13:39:27 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/ranyell/couseSpring/domain/Cliente.java
+++ src/main/java/com/ranyell/couseSpring/domain/Cliente.java
@@ -50,7 +50,7 @@ public class Cliente implements Serializable {
 		this.nome = nome;
 		this.email = email;
 		this.cpfoucnpj = cpfoucnpj;
-		this.tipo = tipo.getCod();
+		this.tipo = (tipo==null)? null : tipo.getCod();
 	}
 
 	public Integer getId() {
