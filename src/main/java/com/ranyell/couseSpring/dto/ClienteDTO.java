3aec79d73c806661c04814217b33c4ab1d6813a9
RanyellHenrique <ranyellhenrique@gmail.com>
2020-02-17 15:39:06 -0300

    Validacao customizada de Atualizacao de clientes

--- src/main/java/com/ranyell/couseSpring/dto/ClienteDTO.java
+++ src/main/java/com/ranyell/couseSpring/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.ranyell.couseSpring.domain.Cliente;
+import com.ranyell.couseSpring.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
