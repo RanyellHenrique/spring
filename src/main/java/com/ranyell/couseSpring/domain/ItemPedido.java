20e0dade068a8b101cc32889483c79707b708e2b
RanyellHenrique <ranyellhenrique@gmail.com>
2020-02-19 12:42:51 -0300

    Sub total e total do pedido

--- src/main/java/com/ranyell/couseSpring/domain/ItemPedido.java
+++ src/main/java/com/ranyell/couseSpring/domain/ItemPedido.java
@@ -32,6 +32,9 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
 
 	public ItemPedidoPK getId() {
 		return id;
