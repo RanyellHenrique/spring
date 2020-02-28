8cfe7559373c71e5ffbdb714ea8b9bc739102219
RanyellHenrique <ranyellhenrique@gmail.com>
2020-02-28 16:36:24 -0300

    Implementando autorizacao

--- src/main/java/com/ranyell/couseSpring/config/SecurityConfig.java
+++ src/main/java/com/ranyell/couseSpring/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.ranyell.couseSpring.security.JWTAuthenticationFilter;
+import com.ranyell.couseSpring.security.JWTAuthorizationFilter;
 import com.ranyell.couseSpring.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
