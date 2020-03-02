bf8524faca790dd0653288b620be6f242f8f02f6
RanyellHenrique <ranyellhenrique@gmail.com>
2020-03-02 01:07:00 -0300

    Enviando imagem via endpoint

--- src/main/java/com/ranyell/couseSpring/CourseSpringApplication.java
+++ src/main/java/com/ranyell/couseSpring/CourseSpringApplication.java
@@ -1,17 +1,11 @@
 package com.ranyell.couseSpring;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.ranyell.couseSpring.services.S3Service;
-
 @SpringBootApplication
 public class CourseSpringApplication implements CommandLineRunner {
-		
-	@Autowired
-	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseSpringApplication.class, args);
@@ -19,7 +13,6 @@ public class CourseSpringApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("/home/ranyell/Downloads/imagem.png");
 	}
 
 }
--- src/main/java/com/ranyell/couseSpring/services/ClienteService.java
+++ src/main/java/com/ranyell/couseSpring/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.ranyell.couseSpring.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ranyell.couseSpring.domain.Cidade;
 import com.ranyell.couseSpring.domain.Cliente;
@@ -38,6 +40,9 @@ public class ClienteService {
 	
 	@Autowired
 	private BCryptPasswordEncoder pe;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find(Integer id) {
 		
@@ -106,6 +111,10 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 
 
