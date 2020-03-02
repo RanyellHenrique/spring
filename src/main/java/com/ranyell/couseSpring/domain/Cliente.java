5ba07fec279671853fdce4b72ae0192494c44c7d
RanyellHenrique <ranyellhenrique@gmail.com>
2020-03-02 01:51:45 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/ranyell/couseSpring/domain/Cliente.java
+++ src/main/java/com/ranyell/couseSpring/domain/Cliente.java
@@ -54,8 +54,6 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	private String imageUrl;
-	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -151,14 +149,6 @@ public class Cliente implements Serializable {
 		this.pedidos = pedidos;
 	}
 	
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
