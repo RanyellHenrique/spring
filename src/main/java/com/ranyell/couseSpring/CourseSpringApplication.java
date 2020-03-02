75bbf90b8c236af7cbeedf3b38e688f0f80df7d1
RanyellHenrique <ranyellhenrique@gmail.com>
2020-03-02 00:33:20 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/ranyell/couseSpring/CourseSpringApplication.java
+++ src/main/java/com/ranyell/couseSpring/CourseSpringApplication.java
@@ -1,12 +1,17 @@
 package com.ranyell.couseSpring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ranyell.couseSpring.services.S3Service;
+
 @SpringBootApplication
 public class CourseSpringApplication implements CommandLineRunner {
-	
+		
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseSpringApplication.class, args);
@@ -14,7 +19,7 @@ public class CourseSpringApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("/home/ranyell/Downloads/imagem.png");
 	}
 
 }
